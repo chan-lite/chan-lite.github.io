5972fa385c997e22dd91cb5150b008cdf81ad99e
Evan Jones <evanjones4040@gmail.com>
2017-05-13 19:02:33 -0700

    fixing styles error

--- src/styles/index.js
+++ src/styles/index.js
@@ -27,24 +27,20 @@ export const GlobalStyles = injectGlobal`
   .quote {
     color: #789922;
   }
-
   .ms-DocumentCard {
     max-width: 99999999px !important;
     min-width: 0 !important;
-
     .ms-DocumentCardTitle {
       padding: 15px 15px 0;
       height: auto;
     }
-
     .ms-DocumentCardActivity {
       padding: 15px;
     }
-    
     .ms-DocumentCardActivity-details {
       top: 15px;
     }
-
+  }
   .postRootContainer {
     .ms-DocumentCard {
       min-width: 0;
@@ -52,36 +48,29 @@ export const GlobalStyles = injectGlobal`
       max-width: 99999999px;
     }
   }
-  
   .highlightedPost {
     .ms-DocumentCard {
       border-color: #0078d7;
-      -webkit-box-shadow: 0px 0px 15px 0px rgba(0,120,215,1);
-      -moz-box-shadow: 0px 0px 15px 0px rgba(0,120,215,1);
-      box-shadow: 0px 0px 15px 0px rgba(0,120,215,1);
+      -webkit-box-shadow: 0px 0px 15px 0px rgba(0, 120, 215, 1);
+      -moz-box-shadow: 0px 0px 15px 0px rgba(0, 120, 215, 1);
+      box-shadow: 0px 0px 15px 0px rgba(0, 120, 215, 1);
     }
   }
-  
-  .buttonRootContainer  {
-
+  .buttonRootContainer {
     .ms-SearchBox {
       margin-bottom: 15px;
-
       .ms-SearchBox-field {
         color: white;
       }
     }
-
     .ms-Button--compound {
       width: 100%;
       max-width: 99999px;
       overflow: hidden;
-
       .ms-Button-label {
         margin-bottom: 0;
         padding-bottom: 5px;
       }
-
       .ms-Button-label,
       .ms-Button-description {
         overflow: hidden;
@@ -90,26 +79,21 @@ export const GlobalStyles = injectGlobal`
       }
     }
   }
-  
   .ms-Breadcrumb {
     margin: 0 0 15px !important;
-
     .ms-Breadcrumb-chevron {
       margin: 0 15px;
       color: white;
       margin-top: 10px;
-
       @media (max-width: 640px) {
         margin-top: 8px;
         margin-left: 0;
         margin-right: 0;
       }
     }
-
     .ms-Link {
       padding: 0 15px 5px !important;
       color: white !important;
-
       &:hover {
         color: black !important;
       }
